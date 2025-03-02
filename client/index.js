f2dd3398b73af764039104fb06cfe35ec99a1bbd
fikre drereg <shewamaretsegaye@gmail.com>
2025-03-02 20:57:52 +0300

    first commit

---
+++ client/index.js
@@ -0,0 +1,239 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express, { response } from "express";
+import bodyParser from "body-parser";
+import axios from 'axios';
+import md5 from "md5";
+import multer from "multer";
+import imgbbUploader from 'imgbb-uploader';
+
+const app = express();
+const port = process.env.CLIENT_PORT;
+const serverURL = `http://localhost:${process.env.SERVER_PORT}`;
+
+let users = []
+let messages = []
+
+app.use(express.json())
+app.use(express.static("public"));
+const upload = multer({dest:'posts/'})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", 'ejs' )
+
+app.get("/", (req, res) => {
+  res.render('start.ejs')
+});
+
+app.get("/login",(req,res)=>{
+  res.render("start",{
+    login:1
+  })
+})
+
+app.get("/signup",(req,res)=>{
+  res.render("start",{
+    signup:1
+  })
+})
+
+const getUsers = async(sender)=>{
+  const result = await axios.get(serverURL+`/users?sender=${sender}`);
+  users = result.data;
+}
+const getAllUsers = async()=>{
+  const result = await axios.get(serverURL+`/allusers`);
+  users = result.data;
+}
+const getMessages = async(sender)=>{
+  const result = await axios.get(serverURL+`/messages?sender=${sender}`);
+  return result.data;
+}
+app.post("/changepp",upload.single('picture'),async(req,res)=>{
+  const img = await imgbbUploader(process.env.IMGBB_API_KEY,req.file.path)
+
+  axios.post(serverURL+"/changepp",{
+    pp:img.url,
+    name:req.body.username
+  }).then(async(response)=>{
+    if(response.error){
+      res.render("changePP",{error:"failed to change try again"})
+    }else{
+      await getAllUsers();
+      let user;
+      users.forEach(usr => {
+        if(usr.name.toLowerCase() == req.body.username.toLowerCase()){
+          user = usr
+        }
+      });
+      await getUsers(req.body.username)
+      res.render("index",{
+        users: users,
+        user: user,
+        messages: messages
+      })
+    }
+  }).catch(err=>res.render("changePP",{error:"failed to change try again"}))
+})
+
+app.post("/login",async(req,res)=>{
+  const name = req.body.username;
+  const password = req.body.password;
+  //check if the user is in the database and render the index
+  let found = false;
+  let activeUser;
+  await getAllUsers();
+  users.forEach(user => {
+    if(user.name.toLowerCase() == name.toLowerCase()){
+      found = true
+      activeUser = user
+    }
+  });
+  if(found){
+    await axios.post(`${serverURL}/logIn?user=${activeUser.name}`)
+    .then(()=>console.log("loged In"))
+    .catch(err=>console.log(err))
+    await getUsers(name)
+    if(activeUser.password == md5(password)){
+      res.render('index',{
+        users:users,
+        user:activeUser,
+        messages:messages
+      })
+    }else{
+      res.render('start',{
+        login:1,
+        error:"incorrect password. Try again"
+      })
+    }
+  }else{
+    res.render('start',{
+      login:1,
+      error:"incorrect username. Try again"
+    })
+  }
+})
+
+app.post("/signup",async(req,res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+  if(password == req.body['password-validation']){
+    await getAllUsers()
+    let userAlreadyIn = false
+    users.forEach(user=>{
+      if(user.name == username){
+        userAlreadyIn = true
+      }
+    })
+    if(!userAlreadyIn){
+      const newUser = {
+        name:username,
+        password: password,
+        pp:"https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg",
+        status:"online"
+      }
+      try{
+        const result = await axios.post(serverURL+"/adduser",{
+          newUser:newUser
+        })
+        await getUsers(username);
+        res.render("index",{
+          users: users,
+          user: newUser,
+          messages: messages
+        })
+      }catch{
+        res.render("start",{
+          signup:1,
+          error:"Error signing you up. Try again"
+        })
+      }
+    }else{
+      res.render("start",{
+        signup:1,
+        error:"The username is already taken. try another"
+      })
+    }
+  }else{
+    res.render("start",{
+      signup:1,
+      error:"Passwords are not the same. Try again"
+    })
+  }
+})
+
+app.get("/getmessages",async(req,res)=>{
+  messages = await getMessages(req.query.sender)
+  res.json(messages)
+})
+
+app.post("/addmessage",async(req,res)=>{
+  try{
+    const newMsg = req.body;
+    await axios.post(serverURL+"/addmessage",newMsg)
+  }catch(err){
+    console.log(err)
+  }
+})
+
+app.post("/updateuser",async(req,res)=>{
+  const oldName = req.body.oldName
+  const newName = req.body.newName
+  await axios.post(serverURL+`/updateUser?oldName=${oldName}&newName=${newName}`)
+  res.status(201).json({message:"user updated successfully."})
+})
+
+app.post("/deleteUser",async(req,res)=>{
+  try{
+    await axios.post(`${serverURL}/deleteUser?user=${req.body.user}`)
+    .then(()=>console.log("deleted"))
+    .catch(err=>console.log(err))
+    res.redirect("/")
+  }catch(err){
+    res.json({message:err.message})
+  }
+})
+app.post("/logOut",async(req,res)=>{
+  try{
+    await axios.post(`${serverURL}/logOut?user=${req.body.user}`)
+    .then(()=>console.log("loged out"))
+    .catch(err=>console.log(err))
+    res.redirect("/")
+  }catch(err){
+    res.json({message:err.message})
+  }
+})
+app.get("/changePassword",(req,res)=>{
+  res.render("changePassword",{
+    user:req.query.user
+  });
+})
+
+app.post("/changePassword",async(req,res)=>{
+  try{
+    const response = await axios.post(serverURL+"/changePassword",req.body);
+    if (response.data.error) { 
+      res.render("changePassword", {
+        error: response.data.error,
+        user: req.body.username
+      })
+    }else{
+      res.redirect("/")
+    }
+  }catch(err){
+    res.status(501).json({message:err.message})
+  }
+})
+
+app.get("/changepp",(req,res)=>{
+  res.render("changepp",{
+    user:req.query.name
+  })
+})
+
+
+app.listen(port, (err) => {
+  if (err) console.log(err);
+  else console.log(`running on http://localhost:${port}`);
+});
+
+
---
+++ server/server.js
@@ -0,0 +1,195 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import md5 from "md5";
+import cors from "cors";
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+const mySchema = new mongoose.Schema(
+  {
+    name: String,
+    password: String,
+    pp: String,
+    status: String,
+  },
+  { timestamps: true }
+);
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: String,
+    receiver: String,
+    message: String,
+  },
+  { timestamps: true }
+);
+
+const User = new mongoose.model("user", mySchema);
+const Message = new mongoose.model("message", messageSchema);
+
+const port = process.env.SERVER_PORT;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+const server = http.createServer(app);
+const origin = "http://localhost:3000";
+
+// Enable CORS for Express requests (if needed)
+app.use(cors({ origin: origin, methods: ["GET", "POST"] }));
+
+// Enable CORS for Socket.io
+const io = new Server(server, {
+  cors: {
+    origin: origin,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to " + socket.id);
+
+  socket.on("joinroom", (room) => {
+    socket.join(room);
+    console.log(`${socket.id} joined room ${room}`);
+  });
+
+  socket.on("message", (message, room) => {
+    socket.join(room);
+    socket.to(room).emit("reply", message);
+    console.log(message + " sent to " + room);
+  });
+});
+
+server.listen(port, () => {
+  console.log("listening on port " + port);
+});
+
+app.get("/users", async (req, res) => {
+  const result = await User.find({ name: { $ne: req.query.sender } });
+  res.json(result);
+});
+app.get("/allusers", async (req, res) => {
+  const result = await User.find();
+  res.json(result);
+});
+app.get("/messages", async (req, res) => {
+  const result = await Message.find({
+    $or: [{ sender: req.query.sender }, { receiver: req.query.sender }],
+  });
+  res.json(result);
+});
+
+app.post("/adduser", async (req, res) => {
+  try {
+    const newUser = new User({
+      name: req.body.newUser.name,
+      password: md5(req.body.newUser.password),
+      pp: req.body.newUser.pp,
+      status: req.body.newUser.status,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "user added successfully" });
+  } catch (err) {
+    res.status(500).json({ Error: err.message });
+  }
+});
+
+app.post("/addmessage", async (req, res) => {
+  try {
+    const newMessage = new Message(req.body);
+    await newMessage.save();
+    res.status(201).json({ message: "message added successfully" });
+  } catch (err) {
+    res.status(500).json({ Error: err.message });
+  }
+});
+
+app.post("/updateUser", async (req, res) => {
+  try {
+    await User.updateOne(
+      { name: req.query.oldName },
+      { $set: { name: req.query.newName } }
+    );
+    await Message.updateMany(
+      { sender: req.query.oldName },
+      { $set: { sender: req.query.newName } }
+    );
+    await Message.updateMany(
+      { receiver: req.query.oldName },
+      { $set: { receiver: req.query.newName } }
+    );
+    res.status(201).json({ message: "updated successfully" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/deleteUser", async (req, res) => {
+  try {
+    await User.deleteOne({ name: req.query.user });
+    await Message.deleteMany({
+      $or: [{ sender: req.query.user }, { receiver: req.query.user }],
+    });
+    res.status(201).json({ message: `deleted user: ${req.query.user}` });
+  } catch (err) {
+    res.status(501).json({ message: err.message });
+  }
+});
+
+app.post("/logIn", async (req, res) => {
+  try {
+    await User.updateOne(
+      { name: req.query.user },
+      { $set: { status: "online" } }
+    );
+    res.status(201).json({ message: `logged in user: ${req.query.user}` });
+  } catch (err) {
+    res.status(501).json({ message: err.message });
+  }
+});
+app.post("/logOut", async (req, res) => {
+  try {
+    await User.updateOne(
+      { name: req.query.user },
+      { $set: { status: "offline" } }
+    );
+    res.status(201).json({ message: `logged out user: ${req.query.user}` });
+  } catch (err) {
+    res.status(501).json({ message: err.message });
+  }
+});
+
+app.post("/changePassword", async (req, res) => {
+  if (req.body.newPassword == req.body.CheckPassword) {
+    const user = await User.findOne({ name: req.body.username });
+    if (user.password == md5(req.body.oldPassword)) {
+      await User.updateOne(
+        { name: req.body.username },
+        { $set: { password: md5(req.body.newPassword) } }
+      );
+      res.status(201).json({ message: "Password changed" });
+    } else {
+      res.json({ error: "The old password is wrong" });
+    }
+  } else {
+    res.json({ error: "The new passwords are different" });
+  }
+  console.log(req.body);
+});
+
+app.post("/changepp",async(req,res)=>{
+  try{
+    await User.updateOne({name:req.body.name},{$set:{pp:req.body.pp}})
+    res.status(201).json({message:"profile picture changed successfully"})
+  }catch(err){
+    res.status(501).json({error:"failed to change profile picture"})
+  }
+})
