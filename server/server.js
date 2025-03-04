33a97370a375f1eda4773b1019ce9fd8d318a158
tse-coder <shewamaretsegaye@gmail.com>
2025-03-04 21:57:22 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 const server = http.createServer(app);
 
-const origin = "http://localhost:3000"
+const origin = "https://t-messanger.onrender.com"
 // Enable CORS for Express requests 
 app.use(cors({ origin: origin, methods: ["GET", "POST"] }));
 
